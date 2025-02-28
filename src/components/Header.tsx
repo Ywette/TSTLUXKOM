fb116793e76fdbcc93eb30efef62acef25c2d70c
Iveta Staune <iveta.staune@ses.com>
2025-02-28 12:34:04 +0200

    Update logo path for GitHub Pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,7 +19,7 @@ export function Header() {
                 {/* Logo section */}
                 <div className="flex items-center">
                     <Image
-                        src="/tst-logo-v45deg.svg"
+                        src="/TSTLUXKOM/tst-logo-v45deg.svg"
                         alt="TST LUXKOM Logo"
                         priority
                         height={100}
